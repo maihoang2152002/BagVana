dcc11674d14ad87dad9a910d9bfaed2fc66bd662
ngochai-hcmus <hainguyen13097@gmail.com>
2023-03-23 20:53:09 +0700

    edit cart dao

--- app/src/main/java/com/example/bagvana/DAO/CartDAO.java
+++ app/src/main/java/com/example/bagvana/DAO/CartDAO.java
@@ -17,35 +17,4 @@ import java.util.List;
 
 public class CartDAO {
 
-    private ArrayList<Product> productList = new ArrayList<>();
-    DatabaseReference databaseReferenceCart;
-
-    public CartDAO(String userID) {
-        databaseReferenceCart = FirebaseDatabase.getInstance().getReference("Cart").child(userID);
-        databaseReferenceCart.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                for (DataSnapshot dataSnapshot: snapshot.getChildren()) {
-
-                    Product product = dataSnapshot.getValue(Product.class);
-                    productList.add(product);
-                    Log.e("DAO", String.valueOf(productList.size()));
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-    }
-
-    public ArrayList<Product> getProductList() {
-        Log.e("Product list", String.valueOf(productList.size()));
-        return productList;
-    }
-
-    public void setProductList(ArrayList<Product> productList) {
-        this.productList = productList;
-    }
 }
