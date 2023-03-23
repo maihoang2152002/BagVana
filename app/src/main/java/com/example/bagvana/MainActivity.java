e76842ba0a1d679b029039db6d48557df90261b3
Hoang Mai <maihoang2152002@gmail.com>
2023-03-23 10:06:55 +0700

    Add icon in homepage

--- app/src/main/java/com/example/bagvana/MainActivity.java
+++ app/src/main/java/com/example/bagvana/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.bagvana;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
     }
 }
