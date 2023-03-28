d15d3e38a86b67468648854a68704dc8a5c70d0f
ngochai-hcmus <hainguyen13097@gmail.com>
2023-03-28 13:15:42 +0700

    handle events in cart activity

--- app/src/main/java/com/example/bagvana/Activity/Order/CartActivity.java
+++ app/src/main/java/com/example/bagvana/Activity/Order/CartActivity.java
@@ -2,11 +2,19 @@ package com.example.bagvana.Activity.Order;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import com.example.bagvana.Adapter.CartAdapter;
 import com.example.bagvana.DAO.CartDAO;
@@ -27,12 +35,20 @@ public class CartActivity extends AppCompatActivity {
     private ArrayList<Product> productList;
     private CartAdapter cartAdapter;
     private RecyclerView recycview_cart;
+    private TextView txt_billCost;
+    private RadioButton rad_chooseAll;
+
+    String billCost;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
 
         recycview_cart = findViewById(R.id.recycview_cart);
+        txt_billCost = findViewById(R.id.txt_billCost);
+        rad_chooseAll = findViewById(R.id.rad_chooseAll);
+
 
         recycview_cart.setHasFixedSize(true);
         recycview_cart.setLayoutManager(new LinearLayoutManager(this));
@@ -41,16 +57,39 @@ public class CartActivity extends AppCompatActivity {
 
         cartAdapter = new CartAdapter(this, productList);
 
+        //txt_billCost.setText("billCost");
+
+        rad_chooseAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!rad_chooseAll.isSelected()) {
+                    rad_chooseAll.setChecked(true);
+                    rad_chooseAll.setSelected(true);
+
+                    cartAdapter.setChooseAll(true);
+                } else {
+                    rad_chooseAll.setChecked(false);
+                    rad_chooseAll.setSelected(false);
+
+                    cartAdapter.setChooseAll(false);
+                }
+            }
+        });
+
         recycview_cart.setAdapter(cartAdapter);
 
+        // userID = 1
         DatabaseReference databaseReferenceCart = FirebaseDatabase.getInstance().getReference("Cart").child("1");
         databaseReferenceCart.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
+                productList.clear();
+
                 for (DataSnapshot dataSnapshot: snapshot.getChildren()) {
 
                     Product product = dataSnapshot.getValue(Product.class);
                     productList.add(product);
+                    Log.e("Actitvity",product.getName());
                 }
 
                 cartAdapter.notifyDataSetChanged();
@@ -61,6 +100,7 @@ public class CartActivity extends AppCompatActivity {
 
             }
         });
+
 
     }
 }
--- app/src/main/java/com/example/bagvana/Adapter/CartAdapter.java
+++ app/src/main/java/com/example/bagvana/Adapter/CartAdapter.java
@@ -1,17 +1,23 @@
 package com.example.bagvana.Adapter;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
@@ -20,6 +26,8 @@ import com.example.bagvana.DAO.CartDAO;
 import com.example.bagvana.DTO.Cart;
 import com.example.bagvana.DTO.Product;
 import com.example.bagvana.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.time.Instant;
 import java.time.temporal.TemporalAdjuster;
@@ -30,6 +38,26 @@ public class CartAdapter extends RecyclerView.Adapter<CartAdapter.CartViewHolder
 
     private Context context;
     private ArrayList<Product> productList;
+
+    private boolean chooseAll;
+
+    private int billCost;
+
+    public int getBillCost() {
+        return billCost;
+    }
+
+    public void setBillCost(int billCost) {
+        this.billCost = billCost;
+    }
+
+    public boolean isChooseAll() {
+        return chooseAll;
+    }
+
+    public void setChooseAll(boolean chooseAll) {
+        this.chooseAll = chooseAll;
+    }
 
     public CartAdapter(Context context, ArrayList<Product> productList) {
         this.context = context;
@@ -50,11 +78,9 @@ public class CartAdapter extends RecyclerView.Adapter<CartAdapter.CartViewHolder
             return;
         }
 
-        int amount = product.getAmount();
-        int price = product.getPrice();
-        int total = amount * price;
-
-        Log.e("Adapter",product.getName());
+        final int amount = product.getAmount();
+        final int price = product.getPrice();
+        final int total = amount * price;
 
         Glide.with(this.context)
                 .load(product.getImage())
@@ -65,6 +91,98 @@ public class CartAdapter extends RecyclerView.Adapter<CartAdapter.CartViewHolder
         holder.txt_amount.setText(String.valueOf(product.getAmount()));
         holder.txt_total.setText(String.valueOf(total));
         holder.txt_color.setText(product.getColor());
+
+        // userID = 1
+        DatabaseReference databaseReferenceCart = FirebaseDatabase.getInstance().getReference("Cart").child("1");
+        holder.txt_plus.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                int amountChanged = product.getAmount();
+                amountChanged = amountChanged + 1 ;
+                holder.txt_amount.setText(String.valueOf(amountChanged));
+                product.setAmount(amountChanged); //add to fire-base
+
+                int totalChange = product.getPrice() * amountChanged;
+                holder.txt_total.setText(String.valueOf(totalChange));
+
+                databaseReferenceCart.child(product.getProductID()).setValue(product);
+            }
+        });
+
+        holder.txt_minus.setOnClickListener(new View.OnClickListener() {
+
+            DatabaseReference databaseReferenceCart = FirebaseDatabase.getInstance().getReference("Cart").child("1");
+            @Override
+            public void onClick(View view) {
+                int amountChanged = product.getAmount();
+                if (amountChanged == 1) {
+                    new AlertDialog.Builder(context)
+                            .setMessage("Bạn chắc chắn muốn bỏ sản phẩm này?")
+                            .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    databaseReferenceCart.child(product.getProductID()).removeValue();
+                                }
+                            })
+
+                            .setNegativeButton(android.R.string.no, null)
+                            .setIcon(android.R.drawable.ic_dialog_alert)
+                            .show();
+                }
+                else {
+                    amountChanged = amountChanged - 1;
+                    holder.txt_amount.setText(String.valueOf(amountChanged));
+                    product.setAmount(amountChanged); //add to fire-base
+
+                    int totalChange = product.getPrice() * amountChanged;
+                    holder.txt_total.setText(String.valueOf(totalChange));
+
+                    databaseReferenceCart.child(product.getProductID()).setValue(product);
+                }
+
+            }
+        });
+
+//        if(isChooseAll()) { //Event
+//            holder.rad_product.setChecked(true);
+//            holder.rad_product.setSelected(true);
+//
+//            billCost = 100;
+//
+//            Intent intent = new Intent("choose-all-message");
+//            intent.putExtra("billCost",billCost);
+//
+//            LocalBroadcastManager.getInstance(context).sendBroadcast(intent);
+//        }
+
+        holder.rad_product.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!holder.rad_product.isSelected()) {
+                    holder.rad_product.setChecked(true);
+                    holder.rad_product.setSelected(true);
+
+                    billCost = billCost + product.getAmount() * product.getPrice();
+                } else {
+                    holder.rad_product.setChecked(false);
+                    holder.rad_product.setSelected(false);
+
+                    billCost = billCost - product.getAmount() * product.getPrice();
+                }
+
+                Intent intent = new Intent("choose-all-message");
+                intent.putExtra("billCost",billCost);
+
+                LocalBroadcastManager.getInstance(context).sendBroadcast(intent);
+            }
+        });
+
+//        if(holder.rad_product.isSelected()) {
+//            billCost = billCost + product.getAmount() * product.getPrice();
+//        }
+
+
+//        Log.e("Adapter End", String.valueOf(billCost));
     }
 
     @Override
