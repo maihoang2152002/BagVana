25f7e2c3511c8e34a1cb691195649e04a53f8e66
Hoang Mai <maihoang2152002@gmail.com>
2023-03-29 15:12:29 +0700

    Add event navbar home, create fragment home

--- app/src/main/java/com/example/bagvana/Activity/Home/HomeActivity.java
+++ app/src/main/java/com/example/bagvana/Activity/Home/HomeActivity.java
@@ -1,64 +1,72 @@
 package com.example.bagvana.Activity.Home;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
+import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
+import androidx.fragment.app.Fragment;
+import com.example.bagvana.Activity.Order.CartActivity;
 import com.example.bagvana.Adapter.HomeAdapter;
 import com.example.bagvana.DTO.Product;
 import com.example.bagvana.R;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
+import com.example.bagvana.fragments.AccountFragment;
+import com.example.bagvana.fragments.CartFragment;
+import com.example.bagvana.fragments.FavFragment;
+import com.example.bagvana.fragments.HomeFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 import java.util.ArrayList;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener {
 
     private ArrayList<Product> productList;
     private HomeAdapter homeAdapter;
-    private RecyclerView recycview_home;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        recycview_home = findViewById(R.id.recycview_home);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener(this);
+        loadFragment(new HomeFragment());
+    }
 
-        recycview_home.setHasFixedSize(true);
-        recycview_home.setLayoutManager(new LinearLayoutManager(this));
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-        productList = new ArrayList<>();
-
-        homeAdapter = new HomeAdapter(this, productList);
-
-        recycview_home.setAdapter(homeAdapter);
-
-        DatabaseReference databaseReferenceHome = FirebaseDatabase.getInstance().getReference("Product");
-        databaseReferenceHome.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
-
-                    Product product = dataSnapshot.getValue(Product.class);
-                    productList.add(product);
-                }
-
-                homeAdapter.notifyDataSetChanged();
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
+        Fragment fragment = null;
+        switch (item.getItemId()) {
+            case R.id.menu_home:
+                fragment = new HomeFragment();
+                break;
+            case R.id.menu_fav:
+                fragment = new FavFragment();
+                break;
+            case R.id.menu_account:
+                fragment = new AccountFragment();
+                break;
+            case R.id.menu_cart:
+                Intent myIntent = new Intent(HomeActivity.this, CartActivity.class);
+//                myIntent.putExtras(myBundle);
+                startActivity(myIntent);
+                break;
+        }
+        return loadFragment(fragment);
+    }
+    boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
     }
 }
