a8cc080e939187d450ebafd476ba30fcd088afd4
Hoang Mai <maihoang2152002@gmail.com>
2023-03-30 11:31:42 +0700

    Add View detail product version 1, Link homepage to detail and profile

--- app/src/main/java/com/example/bagvana/Adapter/HomeAdapter.java
+++ app/src/main/java/com/example/bagvana/Adapter/HomeAdapter.java
@@ -1,3 +1,89 @@
+//package com.example.bagvana.Adapter;
+//
+//import android.content.Context;
+//import android.util.Log;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.ImageView;
+//import android.widget.TextView;
+//
+//import androidx.annotation.NonNull;
+//import androidx.recyclerview.widget.RecyclerView;
+//
+//import com.bumptech.glide.Glide;
+//import com.bumptech.glide.load.engine.DiskCacheStrategy;
+//import com.example.bagvana.DTO.Product;
+//import com.example.bagvana.R;
+//import com.example.bagvana.fragments.HomeFragment;
+//
+//import java.util.ArrayList;
+//
+//public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.HomeViewHolder> {
+//
+//    private final Context context;
+//    private final ArrayList<Product> productList;
+//
+//    public HomeAdapter(Context context, ArrayList<Product> productList) {
+//        this.context = context;
+//        this.productList = productList;
+//    }
+//
+//    @NonNull
+//    @Override
+//    public HomeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//        View view = LayoutInflater.from(context).inflate(R.layout.item_home, parent, false);
+//        return new HomeViewHolder(view);
+//    }
+//
+//    @Override
+//    public void onBindViewHolder(@NonNull HomeViewHolder holder, int position) {
+//        Product product = productList.get(position);
+//        if(product == null) {
+//            return;
+//        }
+//
+//        int price = product.getPrice();
+//
+//        Log.e("Adapter",product.getName());
+//
+//        Glide.with(this.context)
+//                .load(product.getImage())
+//                .diskCacheStrategy(DiskCacheStrategy.ALL)
+//                .into(holder.img_product);
+//
+//        holder.txt_name.setText(product.getName());
+//        holder.txt_price.setText(String.valueOf(price));
+//        holder.txt_color.setText(product.getColor());
+//    }
+//
+//    @Override
+//    public int getItemCount() {
+//        if(productList != null) {
+//            return productList.size();
+//        }
+//        return 0;
+//    }
+//
+//    public static class HomeViewHolder extends RecyclerView.ViewHolder {
+//
+//        public ImageView img_product;
+//        public TextView txt_name;
+//        public TextView txt_color;
+//        public TextView txt_price;
+//        public HomeViewHolder(@NonNull View itemView) {
+//
+//            super(itemView);
+//
+//            img_product = itemView.findViewById(R.id.img_product);
+//            txt_name = itemView.findViewById(R.id.txt_name);
+//            txt_color = itemView.findViewById(R.id.txt_color);
+//            txt_price = itemView.findViewById(R.id.txt_price);
+//        }
+//    }
+//
+//}
+
 package com.example.bagvana.Adapter;
 
 import android.content.Context;
@@ -15,6 +101,7 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.example.bagvana.DTO.Product;
 import com.example.bagvana.R;
+import com.example.bagvana.listeners.ItemListener;
 import com.example.bagvana.fragments.HomeFragment;
 
 import java.util.ArrayList;
@@ -23,10 +110,12 @@ public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.HomeViewHolder
 
     private final Context context;
     private final ArrayList<Product> productList;
+    private ItemListener itemListener;
 
-    public HomeAdapter(Context context, ArrayList<Product> productList) {
+    public HomeAdapter(Context context, ArrayList<Product> productList, ItemListener itemListener) {
         this.context = context;
         this.productList = productList;
+        this.itemListener = itemListener;
     }
 
     @NonNull
@@ -39,19 +128,32 @@ public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.HomeViewHolder
     @Override
     public void onBindViewHolder(@NonNull HomeViewHolder holder, int position) {
         Product product = productList.get(position);
-        if(product == null) {
+        if (product == null) {
             return;
         }
 
         int price = product.getPrice();
 
-        Log.e("Adapter",product.getName());
+        Log.e("Adapter", product.getName());
 
         Glide.with(this.context)
                 .load(product.getImage())
                 .diskCacheStrategy(DiskCacheStrategy.ALL)
                 .into(holder.img_product);
 
+//        Glide.with(context).load(productList.get(position).getImage())
+//                        .centerCrop().placeholder(R.drawable.ic_account)
+//                        .into(new CustomTarget<Drawable>() {
+//                            @Override
+//                            public void onResourceReady(@NonNull Drawable resource, @Nullable Transition<? super Drawable> transition) {
+//                                holder.relativeLayout.setBackground(resource);
+//                            }
+//
+//                            @Override
+//                            public void onLoadCleared(@Nullable Drawable placeholder) {
+//
+//                            }
+//                        });
         holder.txt_name.setText(product.getName());
         holder.txt_price.setText(String.valueOf(price));
         holder.txt_color.setText(product.getColor());
@@ -59,18 +161,19 @@ public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.HomeViewHolder
 
     @Override
     public int getItemCount() {
-        if(productList != null) {
+        if (productList != null) {
             return productList.size();
         }
         return 0;
     }
 
-    public static class HomeViewHolder extends RecyclerView.ViewHolder {
+    public class HomeViewHolder extends RecyclerView.ViewHolder {
 
         public ImageView img_product;
         public TextView txt_name;
         public TextView txt_color;
         public TextView txt_price;
+
         public HomeViewHolder(@NonNull View itemView) {
 
             super(itemView);
@@ -79,6 +182,13 @@ public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.HomeViewHolder
             txt_name = itemView.findViewById(R.id.txt_name);
             txt_color = itemView.findViewById(R.id.txt_color);
             txt_price = itemView.findViewById(R.id.txt_price);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    itemListener.OnItemPosition(getAdapterPosition());
+                }
+            });
         }
     }
 
---
+++ app/src/main/java/com/example/bagvana/listeners/ItemListener.java
@@ -0,0 +1,5 @@
+package com.example.bagvana.listeners;
+
+public interface ItemListener {
+    void OnItemPosition(int position);
+}
