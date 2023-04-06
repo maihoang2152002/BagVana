eed95a3f0f169e46b064e8564b135f6175e84773
ngochai-hcmus <hainguyen13097@gmail.com>
2023-04-06 19:41:36 +0700

    Merge branch 'main' of https://github.com/maihoang2152002/BagVana
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/bagvana/DTO/Order.java
    #	app/src/main/java/com/example/bagvana/Utils/Utils.java
    #	app/src/main/res/drawable/ic_back.xml
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/example/bagvana/Utils/Utils.java
+++ app/src/main/java/com/example/bagvana/Utils/Utils.java
@@ -2,6 +2,7 @@ package com.example.bagvana.Utils;
 
 import com.example.bagvana.DTO.Product;
 import com.example.bagvana.DTO.ReceiverInfo;
+import com.example.bagvana.DTO.User;
 import com.example.bagvana.DTO.Voucher;
 
 import java.util.ArrayList;
@@ -11,6 +12,6 @@ public class Utils {
     public static ArrayList<Product> productList = new ArrayList<>();
     public static ReceiverInfo receiverInfo = new ReceiverInfo();
     public static String newAddressID;
-
     public static ArrayList<Voucher> voucherList = new ArrayList<>();
+    public static User _user = new User();
 }
