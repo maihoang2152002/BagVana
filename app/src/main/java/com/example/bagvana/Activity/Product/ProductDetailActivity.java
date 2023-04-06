41abcd52738b7b2bbf3e3caa863909ebadd7d60d
doducduy20127477 <90464550+doducduy20127477@users.noreply.github.com>
2023-04-06 18:11:53 +0700

    Fix conflict

--- app/src/main/java/com/example/bagvana/Activity/Product/ProductDetailActivity.java
+++ app/src/main/java/com/example/bagvana/Activity/Product/ProductDetailActivity.java
@@ -1,9 +1,12 @@
 package com.example.bagvana.Activity.Product;
 
+import static com.example.bagvana.Utils.Utils._user;
+
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -34,6 +37,8 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 
 public class ProductDetailActivity extends AppCompatActivity implements ItemListener {
+    private ImageView imageSelected;
+    private ImageButton imageBtn_fav;
     Product curProduct;
     TextView name, color, price, description;
 
