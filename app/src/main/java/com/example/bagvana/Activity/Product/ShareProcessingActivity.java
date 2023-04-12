233f34fe14b3ee60fa6bd0a2f77e21bc861e2065
doducduy20127477 <90464550+doducduy20127477@users.noreply.github.com>
2023-04-12 18:20:52 +0700

    Share function ver 2

--- app/src/main/java/com/example/bagvana/Activity/Product/ShareProcessingActivity.java
+++ app/src/main/java/com/example/bagvana/Activity/Product/ShareProcessingActivity.java
@@ -1,5 +1,7 @@
 package com.example.bagvana.Activity.Product;
 
+import static android.content.ContentValues.TAG;
+
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.net.Uri;
@@ -9,13 +11,18 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.bagvana.Activity.LoginRegister.SignInActivity;
 import com.example.bagvana.DTO.Product;
 import com.example.bagvana.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.dynamiclinks.FirebaseDynamicLinks;
+import com.google.firebase.dynamiclinks.PendingDynamicLinkData;
 
 public class ShareProcessingActivity extends AppCompatActivity {
 
@@ -75,13 +82,11 @@ public class ShareProcessingActivity extends AppCompatActivity {
             Log.e("ProductID", params[params.length - 1]);
 
             DatabaseReference databaseReferenceProduct = FirebaseDatabase.getInstance().getReference("Product");
-            Log.e("ProductID", "123");
 
             databaseReferenceProduct.addValueEventListener(new ValueEventListener() {
                 @SuppressLint("NotifyDataSetChanged")
                 @Override
                 public void onDataChange(@NonNull DataSnapshot snapshot) {
-                    Log.e("ProductID", "123");
 
                     for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
 
@@ -99,11 +104,71 @@ public class ShareProcessingActivity extends AppCompatActivity {
 
                 }
 
+
                 @Override
                 public void onCancelled(@NonNull DatabaseError error) {
 
                 }
             });
         }
+
+//        FirebaseDynamicLinks.getInstance()
+//                .getDynamicLink(getIntent())
+//                .addOnSuccessListener(this, new OnSuccessListener<PendingDynamicLinkData>() {
+//                    @Override
+//                    public void onSuccess(PendingDynamicLinkData pendingDynamicLinkData) {
+//                        Log.e("ShareprocessActivity", "We have a dynamic link");
+//                        // Get deep link from result (may be null if no link is found)
+//                        Uri deepLink = null;
+//                        if (pendingDynamicLinkData != null) {
+//                            deepLink = pendingDynamicLinkData.getLink();
+//                        }
+//
+//                        Log.e("Deeplink", String.valueOf(deepLink));
+//                        Intent intent = new Intent(ShareProcessingActivity.this, SignInActivity.class);
+//                        startActivity(intent);
+////                        if (deepLink != null) {
+////                            String path = deepLink.toString();
+////                            String[] params = path.split("/");
+////                            Log.e("ProductID", params[params.length - 1]);
+////
+////                            DatabaseReference databaseReferenceProduct = FirebaseDatabase.getInstance().getReference("Product");
+////
+////                            databaseReferenceProduct.addValueEventListener(new ValueEventListener() {
+////                                @SuppressLint("NotifyDataSetChanged")
+////                                @Override
+////                                public void onDataChange(@NonNull DataSnapshot snapshot) {
+////
+////                                    for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+////
+////                                        Product temp = dataSnapshot.getValue(Product.class);
+////
+////                                        if (params[params.length - 1].equals(temp.getProductID())) {
+////                                            curProduct[0] = temp;
+////                                            Intent intent = new Intent(ShareProcessingActivity.this, ProductDetailActivity.class);
+////                                            intent.putExtra("product", curProduct[0]);
+////                                            startActivity(intent);
+////                                            break;
+////                                        }
+////
+////                                    }
+////
+////                                }
+////
+////                                @Override
+////                                public void onCancelled(@NonNull DatabaseError error) {
+////
+////                                }
+////                            });
+////                        }
+//                    }
+//                })
+//                .addOnFailureListener(this, new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        Log.e(TAG, "getDynamicLink:onFailure", e);
+//                    }
+//                });
     }
-}
+
+    }
